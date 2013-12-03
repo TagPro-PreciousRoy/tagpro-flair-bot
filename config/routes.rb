098e05a801c8d05be14e93bb04dac99a487f1ed2
Benjamin Manns <benmanns@gmail.com>
2013-12-03 01:33:32 -0500

    Create StaticController.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ TagProBot::Application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
+  root to: 'static#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
