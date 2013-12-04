d428b166f930e325d7ff8f02269ca9e73be5c529
Benjamin Manns <benmanns@gmail.com>
2013-12-03 21:30:41 -0500

    Set flair when User is saved.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :database_authenticatable, :lockable, :recoverable, :registerable, :rememberable, :timeoutable and :validatable
@@ -8,9 +10,21 @@ class User < ActiveRecord::Base
 
   has_one :tag_pro_profile, dependent: :destroy
 
+  after_save :write_flair_to_reddit, if: :flair_set?
+
   def self.find_or_create_for_oauth(auth)
     find_or_create_by provider: auth.provider, uid: auth.uid do |user|
       user.name = auth.extra.raw_info.name
     end
+  end
+
+  def flair_set?
+    flair_class || flair_text
+  end
+
+  def write_flair_to_reddit
+    reddit = Snoo::Client.new
+    reddit.log_in ENV['REDDIT_MOD_USERNAME'], ENV['REDDIT_MOD_PASSWORD']
+    reddit.flair_csv [name, flair_text, flair_class].to_csv, ENV['REDDIT_MOD_SUBREDDIT']
   end
 end
