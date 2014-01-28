83ab3f5235fdbe3b6dc654ebbc09aee51f62ea82
Benjamin Manns <benmanns@gmail.com>
2014-01-28 11:31:54 -0800

    Store session secret in ENV.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-TagProBot::Application.config.secret_key_base = 'c9ae33e3bcf4fd97330a826571f91105ef0f47ea3165d87c015437dba15fd5899eb64416fb71dafa3fc49b9ef01f480ad33631a28cb1893f05208fd3be994f02'
+TagProBot::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
